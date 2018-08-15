61104119a36e9629ee30d1d731d41e45723f2240
Hugo Tsuda <hugotsuda@gmail.com>
2018-08-15 13:41:31 -0300

    Meu primeiro commit

---
+++ models/User.js
@@ -0,0 +1,14 @@
+class User{
+
+    constructor(name, gender, birthday, country, email, password, photo, admin){
+        this.name = name;
+        this.gender = gender;
+        this.birthday = birthday;
+        this.country = country;
+        this.email = email;
+        this.password = password;
+        this.photo = photo;
+        this.admin = admin;
+    }
+
+}
